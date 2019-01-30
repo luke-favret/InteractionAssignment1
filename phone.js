96060b846b8c4d95b0629238780d8b7dacc94514
luke-favret <45835843+luke-favret@users.noreply.github.com>
2019-01-30 16:26:47 -0700

    Add files via upload

--- phone.js
+++ phone.js
@@ -1,22 +1,22 @@
 $(document).ready(function () { // do this when the document is loaded
     "use strict";
-	#dialerPage.style.display = 'none';  //$("#dialerPage").show(); // show the element with ID "element"
+	$("#dialerPage").show(); // show the element with ID "element"
 	$("#contactPage").hide(); // hide the element with ID "otherElement"
     $("#addPage").hide();
 });
-$("dialTab").click(function () { // when "button_id" is clicked
+$("#dialTab").click(function () { // when "button_id" is clicked
     "use strict";
 	$("#dialerPage").show(); // show element
 	$("#contactPage").hide();	// hide other element
     $("#addPage").hide();
 });
-$("contactTab").click(function () { // when "button_id" is clicked
+$("#contactTab").click(function () { // when "button_id" is clicked
     "use strict";
 	$("#contactPage").show(); // show element
 	$("#dialerPage").hide();	// hide other element
     $("#addPage").hide();
 });
-$("addTab").click(function () { // when "button_id" is clicked
+$("#addTab").click(function () { // when "button_id" is clicked
     "use strict";
 	$("#addPage").show(); // show element
 	$("#dialerPage").hide();	// hide other element
