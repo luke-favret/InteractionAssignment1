209dd837e69bea3aeae5ee2dce42600f70d67ef5
luke-favret <45835843+luke-favret@users.noreply.github.com>
2019-02-12 21:57:00 -0700

    Add files via upload
    Accidentally pushed to wrong repository so reuploading old files

--- phone.js
+++ phone.js
@@ -3,43 +3,22 @@ $(document).ready(function () { // do this when the document is loaded
 	$("#dialerPage").show(); // show the element with ID "element"
 	$("#contactPage").hide(); // hide the element with ID "otherElement"
     $("#addPage").hide();
-    $("#helpPage").hide();
 });
 $("#dialTab").click(function () { // when "button_id" is clicked
     "use strict";
 	$("#dialerPage").show(); // show element
 	$("#contactPage").hide();	// hide other element
     $("#addPage").hide();
-    $("#helpPage").hide();
 });
 $("#contactTab").click(function () { // when "button_id" is clicked
     "use strict";
 	$("#contactPage").show(); // show element
 	$("#dialerPage").hide();	// hide other element
     $("#addPage").hide();
-    $("#helpPage").hide();
 });
 $("#addTab").click(function () { // when "button_id" is clicked
     "use strict";
 	$("#addPage").show(); // show element
 	$("#dialerPage").hide();	// hide other element
     $("#contactPage").hide();
-    $("#helpPage").hide();
-});
-$("#helpTab").click(function () { // when "button_id" is clicked
-    "use strict";
-	$("#addPage").hide(); 
-	$("#dialerPage").hide();	
-    $("#contactPage").hide();
-    $("#helpPage").show();
-});
-
-
-/* fancy dialing functions */
-$(".padNum").click(function () {
-	$("#dialInput").val($("#dialInput").val() + this.innerText);
-});
-
-$("#padClear").click(function () {
-	$("#dialInput").val("");
 });
